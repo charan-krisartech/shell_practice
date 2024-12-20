d8651a282a7221781b35abfbe89c889e6067af62
charan-krisartech <charan@krisar.tech>
2024-12-20 17:26:07 +0530

    Update grep3.sh

--- shell/hackerrank/grep3.sh
+++ shell/hackerrank/grep3.sh
@@ -1,2 +1,4 @@
+# Previious grep problemes have something similar with fewer options - 3 problems
+
 # https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-3/problem?isFullScreen=true
 grep -hwiv that $1
